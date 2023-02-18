ba84d28240c88e9b0f1d70fec27a95b781381f05
newell212 <michaelnewell@michaels-mbp.lan>
2023-02-18 14:45:36 -0700

    added handleChange

--- src/pizza.js
+++ src/pizza.js
@@ -1,7 +1,13 @@
 import React, {useState} from 'react';
 
 export default function PizzaForm() {
-const [order, setOrder] = useState({name: "",size: "", topping1: 0, topping2: 0, special: ""});
+const [order, setOrder] = useState({name: "",size: "", pepperoni: false, sausage: false, mushrooms: false, peppers: false, special: ""});
+
+const handeChange = event => {
+    const {name, type, value, checked} = event.target;
+    const updatedInfo = type === "checkbox" ? checked: value;
+    setOrder({...order, [name]: updatedInfo});
+}
 
     return(
         <form id = 'pizza-form' onSubmit={handleSubmit}>
@@ -22,7 +28,42 @@ const [order, setOrder] = useState({name: "",size: "", topping1: 0, topping2: 0,
                 <option value="LAR">Large</option>
                 <option value="XLAR">X-Large</option>
                 </select>
-            </label>    
+            </label> 
+            <label>Select Your Toppings
+                <input
+                    name="pepperoni"
+                    type="checkbox"
+                    checked="order.pepperoni"
+                    onChange={handleChange}
+                    />
+                <input
+                    name="sausage"
+                    type="checkbox"
+                    checked="order.sausage"
+                    onChange={handleChange}
+                    />
+                <input
+                    name="mushrooms"
+                    type="checkbox"
+                    checked="order.mushrooms"
+                    onChange={handleChange}
+                    />
+                <input
+                    name="peppers"
+                    type="checkbox"
+                    checked="order.peppers"
+                    onChange={handleChange}
+                    />    
+            </label>  
+            <label>Special Instructions
+                <input
+                    type="text"
+                    id="special-text"
+                    name="special"
+                    checked="order.special"
+                    onChange={handleChange}
+                    />
+            </label> 
             </div>  
         </form>
     )
